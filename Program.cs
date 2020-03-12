9cc5677e8f4d16c3549e01f6d017400c1d91b2a7
Boris Rumyantsev <bdrum@jinr.ru>
2020-03-12 18:39:47 +0300

    Realised squirrel.windows problems for console app

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
 ﻿using System;
+using System.Diagnostics;
+
+/*
+dotnet.exe build -c Release
+nuget pack .\TestAutoDeploy.nuspec
+packages\squirrel.windows.1.9.1\tools\Squirrel.exe --no-delta --no-msi -releasify TestAutoDeploy.1.3.0.nupkg
+
+
+*/
 
 namespace TestAutoSquirrel
 {
@@ -6,7 +15,10 @@ namespace TestAutoSquirrel
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("");
+      System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+      FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+      Console.WriteLine($"Here is TestApp-{fvi.FileVersion}. Press any 'enter' for exit:");
+      Console.ReadLine();
     }
   }
 }
